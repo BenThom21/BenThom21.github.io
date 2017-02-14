3f1f0ece049cb8252ddd419ff8aa1d603c64ce45
Ben Thompson <benthompson4496@gmail.com>
2017-02-14 06:39:24 -0600

    adding scripts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,50 @@
-"<script>" 
+<script>
+
+  // closing navbar on click -- since it stays open on its own //
+  $(document).on('click',function(){
+  $('.collapse').collapse('hide');
+  })
+  //Smooth scrolling
+  $(document).ready(function(){
+  	$('a[href^="#"]').on('click',function (e) {
+  	    e.preventDefault();
+  	    var target = this.hash,
+  	    $target = $(target);
+  	    $('html, body').stop().animate({
+  	        'scrollTop': $target.offset().top
+  	    }, 900, 'swing')
+  	});
+  });
+  // Adding Animate.css class when scrolled into viewport
+  function isScrolledIntoView(elem) {
+      var $window = $(window),
+          docViewTop = $window.scrollTop(),
+          docViewBottom = docViewTop + $window.height(),
+          elemTop = $(elem).offset().top,
+          elemBottom = elemTop + $(elem).outerHeight() - 200;
+      return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+
+  $(window).on("scroll", function() {
+
+      $('.fade').each(function() {
+          if (isScrolledIntoView(this)) {
+              $(this).addClass('animated fadeInUp');
+          }
+
+      });
+  });
+  // Changing the color of the navbar when scrolled
+  jQuery(document).ready(function($) {
+    $(window).scroll(function() {
+      var scrollPos = $(window).scrollTop(),
+          navbar = $('.navbar-default');
+
+      if (scrollPos > 20) {
+        navbar.addClass('change-color');
+      } else {
+        navbar.removeClass('change-color');
+      }
+    });
+  });
+</script>
